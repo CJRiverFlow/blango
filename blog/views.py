1259ace0f974b97ea96cc4909981d21f041cb5dc
Christian Flores <christian.rivera@hotmail.es>
2022-02-06 23:56:43 +0000

    temp save during caching

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+# from django.views.decorators.cache import cache_page
+# from django.views.decorators.vary import vary_on_cookie
 
 from blog.models import Post
 logger = logging.getLogger(__name__)
 
 
+# @cache_page(300)
+# @vary_on_cookie
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts)) 
